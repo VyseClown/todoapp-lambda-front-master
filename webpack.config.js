60782a82434fa1eab6fd882fd530bc6f1b8ed1af
Alessandro <vyseclown@gmail.com>
2020-02-06 23:15:37 -0300

    Criando o index.html necessário

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
+  devtool: "inline-source-map",
   // define entry file and output
   entry: "./src/index.js",
   output: {
     path: path.resolve("dist"),
-    filename: "main.js"
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./build"
   },
   // define babel loader, damn has been months since I needed to config this haha
   module: {
     rules: [
       { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./index.html")
+    })
+  ]
 };
